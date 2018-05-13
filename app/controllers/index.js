a8d61f7f13f420912038a4531c41f9d41133eeb9
Draven072 <mdmitry072@gmail.com>
2018-05-13 22:10:59 +0300

    computed property isDisabled: empty() added to index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,27 @@
-import { computed, observer } from '@ember/object';
+//import { computed, observer } from '@ember/object';
+//import { computed } from '@ember/object';
 import Controller from '@ember/controller';
+import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
 
-  isDisabled: true,
+//  isDisabled: true,
 
   emailAddress: '',
 
-  actualEmailAddress: computed('emailAddress', function() {
+  isDisabled: empty('emailAddress')
+
+/*
+  isDisabled: computed('emailAddress', function() {
+    return this.get('emailAddress') === '';
+  })
+*/
+/*  actualEmailAddress: computed('emailAddress', function() {
     console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
   }),
 
   emailAddressChanged: observer('emailAddress', function() {
     console.log('observer is called', this.get('emailAddress'));
   })
-  
+*/
 });
